7f306aca3a7b4c5cfcf82c1120c69312b3ae774e
Jose <jos1991fernando@gmail.com>
2021-09-01 13:34:23 -0500

    primer commit del ecommerce

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import 'package:viking_store/src/login/login_page.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+ const MyApp({Key? key}) : super(key: key);
+
+  @override
+   _MyAppState createState() =>  _MyAppState();
+}
+
+class  _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'Delivery App Flutter',
+      initialRoute: 'login',
+      routes:  {
+        'login': (BuildContext context) => LoginPage()
+      },
+    );
+  }
+}
